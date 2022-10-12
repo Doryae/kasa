55398979488ee4a8753f38b390cb977623c83488
Taverniers Dylan <dylan.taverniers@gmail.com>
2022-10-12 20:55:09 +0200

    Organisation Projet + page 404 débutée
    Organisation du projet : 
    - Suppression de la plupart des fichiers ne servant pas.
    - Organisation de la structure des dossiers / fichiers
    - Ajout de la font utilisée en l'incluant dans le CSS
    - Préparation d'un CSS "propre". 
    - NavBar essentiellement terminée --> Aura besoin de devenir un component une fois qu'elle sera terminée.
    - Système de Routing en place. 
    
    Page 404 :
    - Réalisée en taille mobile. Reste à faire le responsive + les autres pages.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style/style.css"
+import { Routes, Route, Link } from "react-router-dom"
+import NotFound from "./components/pages/NotFound"
+import Logo from "./assets/img/logo.png"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav>
+          <img src={Logo} alt="Logo de Kasa" />
+          <div>
+            <Link className="c-main" to="/">
+              Accueil
+            </Link>
+
+            <Link className="c-main" to="/404">
+              A Propos
+            </Link>
+          </div>
+        </nav>
       </header>
+      <Routes>
+        <Route path="/" />
+        <Route path="/404" element={<NotFound />} />
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+const NotFound = () => {
+  return (
+    <div className="container-404 c-main">
+      <div>
+        <h2>404</h2>
+        <p>Oups! La page que vous demandez n'existe pas.</p>
+      </div>
+      <Link className="c-main" to="/">
+        Retourner sur la page d'accueil
+      </Link>
+    </div>
+  )
+}
+
+export default NotFound
